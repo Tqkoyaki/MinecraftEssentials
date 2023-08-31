b64c15d02c5ef6ba346bf9e68cd9139318999e51
Tqkoyaki <np768@scarletmail.rutgers.edu>
2023-08-31 13:53:24 -0400

    First Commit

---
+++ src/main/java/com/takoyaki/essentials/FabricModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.takoyaki.essentials;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FabricModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
